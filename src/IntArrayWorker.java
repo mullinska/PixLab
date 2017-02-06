50b054610b5f0f598e46aaa87fb70421c3c01865
Keegan Alexander Mullins <mullinska@dcsdk12.org>
2017-02-06 12:56:20 -0700

    exercise 2

--- src/IntArrayWorker.java
+++ src/IntArrayWorker.java
@@ -97,5 +97,17 @@ public class IntArrayWorker {
 		return times;
 	}
 	
+	public int getLargest() {
+		int largest = 0;
+		for (int row = 0; row < matrix.length; row++) {
+			for (int col = 0; col < matrix[0].length; col++) {
+				if (largest < matrix[row][col]) {
+					largest = matrix[row][col];
+				}
+			}
+		}
+		return largest;
+	}
+	
 	
 }
