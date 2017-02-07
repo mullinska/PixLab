e0101f1bc1765a9cd4c9be8413343c45d9cd9c96
Keegan Alexander Mullins <mullinska@dcsdk12.org>
2017-02-07 12:25:37 -0700

    questions

---
+++ answers.md
@@ -0,0 +1,15 @@
+Questions:
+
+1: I did not see the method there.
+
+2: Yes
+
+3: No
+
+4: Yes
+
+5: Yes
+
+6: Yes
+
+7: No
--- src/Picture.java
+++ src/Picture.java
@@ -208,6 +208,7 @@ public class Picture extends SimplePicture {
 		beach.explore();
 		beach.zeroBlue();
 		beach.explore();
+		Picture p = new SimplePicture();
 	}
 
 } // this } is the end of class Picture, put all new methods before this
